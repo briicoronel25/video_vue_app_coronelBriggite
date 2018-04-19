2f6c077e38b65565adbfd2abe2ac1715737f755e
Brigitte Coronel <briicoronel25@gmail.com>
2018-04-19 09:53:04 -0500

    Adding design for reviews and for making comments

--- public/javascripts/videos_functions.js
+++ public/javascripts/videos_functions.js
@@ -1,8 +1,15 @@
 var elem = document.querySelector('.sidenav');
-options = { 'edge': 'right' }
+var options = { 'edge': 'right' }
 var instance = M.Sidenav.init(elem, options);
 
 var elem = document.querySelectorAll('.tooltipped');
 console.log(elem);
 opts = {};
 var tooltip_instance = M.Tooltip.init(elem, opts);
+
+var elem = document.querySelector('.fixed-action-btn');
+options = {
+    direction: 'right',
+    hoverEnabled: true
+};
+var instance = M.FloatingActionButton.init(elem, options);
